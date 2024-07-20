4cc97107a6e3683e84d4081d3daeffed7a581f4e
lqtx96 <87886670+lqtx96@users.noreply.github.com>
2024-07-20 09:25:59 +0700

    Updating Tank's software (#2)
    * Updating Tank's software
    
    * Implementing Tank's move function
    
    * Implementing Tank's arm movement

--- Software/Tank/Tank.ino
+++ Software/Tank/Tank.ino
@@ -1,189 +1,205 @@
-#include <PS2X_lib.h>  //for v1.6
+#include <PS2X_lib.h>
+#include <Servo.h>
 
-/******************************************************************
- * set pins connected to PS2 controller:
- *   - 1e column: original 
- *   - 2e colmun: Stef?
- * replace pin numbers by the ones you use
- ******************************************************************/
-#define PS2_DAT        13  //14    
-#define PS2_CMD        11  //15
-#define PS2_SEL        10  //16
-#define PS2_CLK        12  //17
+#define PS2_DAT        51 // MOSI   
+#define PS2_CMD        50 // MISO
+#define PS2_SEL        53 
+#define PS2_CLK        52
 
-/******************************************************************
- * select modes of PS2 controller:
- *   - pressures = analog reading of push-butttons 
- *   - rumble    = motor rumbling
- * uncomment 1 of the lines for each mode selection
- ******************************************************************/
-//#define pressures   true
 #define pressures   false
-//#define rumble      true
 #define rumble      false
 
-PS2X ps2x; // create PS2 Controller Class
+// 9 to 15 PWM pins
 
-//right now, the library does NOT support hot pluggable controllers, meaning 
-//you must always either restart your Arduino after you connect the controller, 
-//or call config_gamepad(pins) again after connecting the controller.
+#define INDICATOR_LED
+#define BUZZER
+
+#define LEFT_WHEEL_FORWARD 2
+#define LEFT_WHEEL_BACKWARD 3
+#define RIGHT_WHEEL_FORWARD 4
+#define RIGHT_WHEEL_BACKWARD 5
+
+#define LOADER_PUSH 6
+#define LOADER_PULL 7
+
+#define LEFT_ROLLER_FIRE 8
+#define LEFT_ROLLER_RELOAD 9
+#define RIGHT_ROLLER_FIRE 10
+#define RIGHT_ROLLER_RELOAD 11
+
+#define ARM_PIN 12
+
+#define INTAKE_FAN_DRAW
+#define INTAKE_FAN_BLOW
+
+#define LOADER_FAN_DRAW
+#define LOADER_FAN_BLOW
+
+#define SPREAD_POSITION 0
+#define FOLD_POSITION 105
+
+#define STICK_THRESHOLD 50
+
+PS2X ps2x;
+Servo arm;
 
 int error = 0;
 byte type = 0;
 byte vibrate = 0;
 
-void setup(){
- 
-  Serial.begin(57600);
+int LY = 0;
+int RX = 0;
+
+int mappedLY = 0;
+int mappedRX = 0;
+
+int leftSpeed = 0;
+int rightSpeed = 0;
+
+void configPinout();
+int configController();
+void move();
+
+void setup() {
+  configPinout();  
+  Serial.begin(115200);  
+  delay(300);
+  configController();
+}
+
+void loop() {
+  if(error == 1) //skip loop if no controller found
+    return; 
   
-  delay(300);  //added delay to give wireless ps2 module some time to startup, before configuring it
-   
-  //CHANGES for v1.6 HERE!!! **************PAY ATTENTION*************
-  
-  //setup pins and settings: GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+  if(type == 2) //Guitar Hero Controller
+    return;
+
+  else { //DualShock Controller
+    ps2x.read_gamepad(false, false); //read controller
+
+    // if(ps2x.Button(PSB_START))
+    //   Serial.println("START");
+    // if(ps2x.Button(PSB_SELECT))
+    //   Serial.println("SELECT");      
+
+    if(ps2x.Button(PSB_PAD_UP)) {
+      Serial.println("PUSH");
+    }
+
+    if(ps2x.Button(PSB_PAD_DOWN)) {
+      Serial.println("PULL");
+    }
+
+    if(ps2x.Button(PSB_PAD_RIGHT)) {        // Can be replace Button() function with ButtonPressed()
+      Serial.println("RELOAD TOGGLED");
+      delay(200);
+    }
+
+    if(ps2x.Button(PSB_PAD_LEFT)) {         // Can be replace Button() function with ButtonPressed()
+      Serial.println("FIRE TOGGLED");
+      delay(200);
+    }
+
+    if(ps2x.Button(PSB_TRIANGLE)) {
+      arm.write(FOLD_POSITION);
+      Serial.println("FOLD UP");
+    }
+
+    if(ps2x.Button(PSB_CROSS)) {
+      arm.write(SPREAD_POSITION);
+      Serial.println("SPREAD OUT");
+    }
+
+    if(ps2x.Button(PSB_SQUARE)) {
+      Serial.println("INTAKE FAN TOGGLED");
+      delay(200);
+    }
+    
+    if(ps2x.Button(PSB_CIRCLE)) {
+      Serial.println("LOADER FAN TOGGLED");
+      delay(200);
+    }
+
+    LY = ps2x.Analog(PSS_LY);
+    RX = ps2x.Analog(PSS_RX);
+    
+    mappedLY = map(LY, 0, 255, 254, -254);
+    mappedRX = map(RX, 0, 255, -254, 254);
+
+    leftSpeed = mappedLY + mappedRX;
+    rightSpeed = mappedLY - mappedRX;
+
+    leftSpeed = constrain(leftSpeed, -254, 254);
+    rightSpeed = constrain(rightSpeed, -254, 254);
+
+    if (abs(leftSpeed) < STICK_THRESHOLD)
+      leftSpeed = 0;
+    if (abs(rightSpeed) < STICK_THRESHOLD)
+      rightSpeed = 0;
+
+    Serial.print(leftSpeed);
+    Serial.print(' ');
+    Serial.println(rightSpeed);
+    
+    move(leftSpeed, rightSpeed);
+  }
+}
+
+void configPinout() {
+  pinMode(LEFT_WHEEL_FORWARD, OUTPUT);
+  pinMode(LEFT_WHEEL_BACKWARD, OUTPUT);
+  pinMode(RIGHT_WHEEL_FORWARD, OUTPUT);
+  pinMode(RIGHT_WHEEL_BACKWARD, OUTPUT);
+
+  arm.attach(ARM_PIN);
+}
+
+int configController() {
   error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
   
-  if(error == 0){
-    Serial.print("Found Controller, configured successful ");
-    Serial.print("pressures = ");
-	if (pressures)
-	  Serial.println("true ");
-	else
-	  Serial.println("false");
-	Serial.print("rumble = ");
-	if (rumble)
-	  Serial.println("true)");
-	else
-	  Serial.println("false");
-    Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
-    Serial.println("holding L1 or R1 will print out the analog stick values.");
-    Serial.println("Note: Go to www.billporter.info for updates and to report bugs.");
-  }  
+  if(error == 0)
+    Serial.print("Controller found, configured successful");
   else if(error == 1)
-    Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
-   
+    Serial.println("No controller found");   
   else if(error == 2)
-    Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
-
+    Serial.println("Controller found but not accepting commands");
   else if(error == 3)
-    Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
-  
-//  Serial.print(ps2x.Analog(1), HEX);
+    Serial.println("Controller found but refusing to enter Pressures mode");
   
   type = ps2x.readType(); 
   switch(type) {
     case 0:
-      Serial.print("Unknown Controller type found ");
+      Serial.print("Unknown Controller");
       break;
     case 1:
-      Serial.print("DualShock Controller found ");
+      Serial.print("DualShock Controller");
       break;
     case 2:
-      Serial.print("GuitarHero Controller found ");
+      Serial.print("GuitarHero Controller");
       break;
-	case 3:
-      Serial.print("Wireless Sony DualShock Controller found ");
+    case 3:
+      Serial.print("Wireless Sony DualShock Controller");
       break;
-   }
+  }
+  return error;
 }
 
-void loop() {
-  /* You must Read Gamepad to get new values and set vibration values
-     ps2x.read_gamepad(small motor on/off, larger motor strenght from 0-255)
-     if you don't enable the rumble, use ps2x.read_gamepad(); with no values
-     You should call this at least once a second
-   */  
-  if(error == 1) //skip loop if no controller found
-    return; 
-  
-  if(type == 2){ //Guitar Hero Controller
-    ps2x.read_gamepad();          //read controller 
-   
-    if(ps2x.ButtonPressed(GREEN_FRET))
-      Serial.println("Green Fret Pressed");
-    if(ps2x.ButtonPressed(RED_FRET))
-      Serial.println("Red Fret Pressed");
-    if(ps2x.ButtonPressed(YELLOW_FRET))
-      Serial.println("Yellow Fret Pressed");
-    if(ps2x.ButtonPressed(BLUE_FRET))
-      Serial.println("Blue Fret Pressed");
-    if(ps2x.ButtonPressed(ORANGE_FRET))
-      Serial.println("Orange Fret Pressed"); 
-
-    if(ps2x.ButtonPressed(STAR_POWER))
-      Serial.println("Star Power Command");
-    
-    if(ps2x.Button(UP_STRUM))          //will be TRUE as long as button is pressed
-      Serial.println("Up Strum");
-    if(ps2x.Button(DOWN_STRUM))
-      Serial.println("DOWN Strum");
- 
-    if(ps2x.Button(PSB_START))         //will be TRUE as long as button is pressed
-      Serial.println("Start is being held");
-    if(ps2x.Button(PSB_SELECT))
-      Serial.println("Select is being held");
-    
-    if(ps2x.Button(ORANGE_FRET)) {     // print stick value IF TRUE
-      Serial.print("Wammy Bar Position:");
-      Serial.println(ps2x.Analog(WHAMMY_BAR), DEC); 
-    } 
+void move(int leftWheelSpeed, int rightWheelSpeed) {
+  if (leftWheelSpeed > 0) {
+    analogWrite(LEFT_WHEEL_FORWARD, leftWheelSpeed);
+    analogWrite(LEFT_WHEEL_BACKWARD, 0);
   }
-  else { //DualShock Controller
-    ps2x.read_gamepad(false, vibrate); //read controller and set large motor to spin at 'vibrate' speed
-    
-    if(ps2x.Button(PSB_START))         //will be TRUE as long as button is pressed
-      Serial.println("Start is being held");
-    if(ps2x.Button(PSB_SELECT))
-      Serial.println("Select is being held");      
-
-    if(ps2x.Button(PSB_PAD_UP)) {      //will be TRUE as long as button is pressed
-      Serial.print("Up held this hard: ");
-      Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
-    }
-    if(ps2x.Button(PSB_PAD_RIGHT)){
-      Serial.print("Right held this hard: ");
-      Serial.println(ps2x.Analog(PSAB_PAD_RIGHT), DEC);
-    }
-    if(ps2x.Button(PSB_PAD_LEFT)){
-      Serial.print("LEFT held this hard: ");
-      Serial.println(ps2x.Analog(PSAB_PAD_LEFT), DEC);
-    }
-    if(ps2x.Button(PSB_PAD_DOWN)){
-      Serial.print("DOWN held this hard: ");
-      Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
-    }   
-
-    vibrate = ps2x.Analog(PSAB_CROSS);  //this will set the large motor vibrate speed based on how hard you press the blue (X) button
-    if (ps2x.NewButtonState()) {        //will be TRUE if any button changes state (on to off, or off to on)
-      if(ps2x.Button(PSB_L3))
-        Serial.println("L3 pressed");
-      if(ps2x.Button(PSB_R3))
-        Serial.println("R3 pressed");
-      if(ps2x.Button(PSB_L2))
-        Serial.println("L2 pressed");
-      if(ps2x.Button(PSB_R2))
-        Serial.println("R2 pressed");
-      if(ps2x.Button(PSB_TRIANGLE))
-        Serial.println("Triangle pressed");        
-    }
-
-    if(ps2x.ButtonPressed(PSB_CIRCLE))               //will be TRUE if button was JUST pressed
-      Serial.println("Circle just pressed");
-    if(ps2x.NewButtonState(PSB_CROSS))               //will be TRUE if button was JUST pressed OR released
-      Serial.println("X just changed");
-    if(ps2x.ButtonReleased(PSB_SQUARE))              //will be TRUE if button was JUST released
-      Serial.println("Square just released");     
-
-    if(ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1)) { //print stick values if either is TRUE
-      Serial.print("Stick Values:");
-      Serial.print(ps2x.Analog(PSS_LY), DEC); //Left stick, Y axis. Other options: LX, RY, RX  
-      Serial.print(",");
-      Serial.print(ps2x.Analog(PSS_LX), DEC); 
-      Serial.print(",");
-      Serial.print(ps2x.Analog(PSS_RY), DEC); 
-      Serial.print(",");
-      Serial.println(ps2x.Analog(PSS_RX), DEC); 
-    }     
+  else {
+    analogWrite(LEFT_WHEEL_FORWARD, 0);
+    analogWrite(LEFT_WHEEL_BACKWARD, abs(leftWheelSpeed));
   }
-  delay(50);  
+
+  if (rightWheelSpeed > 0) {
+    analogWrite(RIGHT_WHEEL_FORWARD, rightWheelSpeed);
+    analogWrite(RIGHT_WHEEL_BACKWARD, 0);
+  }
+  else {
+    analogWrite(RIGHT_WHEEL_FORWARD, 0);
+    analogWrite(RIGHT_WHEEL_BACKWARD, abs(rightWheelSpeed));
+  }
 }
